c60b99f27a1e63f8f14eb7482776f6c16f04d11c
meravZ <meravzaouch@gmail.com>
2019-02-14 18:57:35 +0200

    is logged

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -24,16 +24,18 @@ export class AuthService {
     }
   }
 
-  setToken(t:string){
+  private setToken(t:string){
     window.localStorage.setItem(environment.tokenKey , t); // toutes les choses globales de l apllication il faut mettre dans environnements 
   }
 
-  getToken(){
+  private getToken(){
    return window.localStorage.getItem(environment.tokenKey);
   }
 
-  isLogged(){
-    return this.getToken() !=null ; // si il est identifier alors il y a eu setToken (identification boutsaa beatslaha alors on a fait un setToken) 
+  isLogged() :Observable<boolean>{ // doit etre un observable car c le seul moyen d etre meadken (via subscribe) tous les autres services si 
+                                   // l'utilisateur estt connecte ou non
+                                   // tous les services qui sont connecte a lui l'observable les previent d'un changement
+    return of(this.getToken() !=null) ; // si il est identifier alors il y a eu setToken (identification boutsaa beatslaha alors on a fait un setToken) 
                                     //donc on peut etre mafile la fonction get
   }
 }
